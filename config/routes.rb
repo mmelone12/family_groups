de820f7cc0ebd49fcdfb008271fecae30bc36421
mmelone12 <matt.m.melone@gmail.com>
2013-09-03 18:03:22 -0700

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 FamilyGroups::Application.routes.draw do
-  get "users/new"
+  resources :users
   root 'static_pages#home'
   match '/signup', to: 'users#new', via: 'get'
   match '/team', to: 'static_pages#team', via: 'get'
