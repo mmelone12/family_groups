7609f67401c66f745170f92ead64a242417ab537
mmelone12 <matt.m.melone@gmail.com>
2013-09-13 17:17:57 -0700

    figured out hiding

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
   	@user = current_user
-  	@city = request.location.city
-  	@interests = if current_user.following != nil then
-  					Interest.where("id NOT IN (?)", current_user.relationships.pluck(:followed_id))
-  				else Interest.all
-  				end
+  	@city = request.location.city    	
+  	@interests = Interest.all
   end
 
   def team
