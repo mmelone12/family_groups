97cdbaec120fb1f65681ded61f2123190d54b093
mmelone12 <matt.m.melone@gmail.com>
2013-09-18 23:02:04 -0700

    Added Random Groups Images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+	def random_image
+  		image_files = %w( .jpg .gif .png )
+  		files = Dir.entries(
+    	"app/assets/images/rotate/" 
+  		).delete_if { |x| !image_files.index(x[-4,4]) }
+  		files[rand(files.length)]
+	end
+
+	def random_image1
+  blacklist = [".", ".."]
+  file_names = Dir.glob("app/assets/images/rotate/*")
+  blacklist.each do |blacklsited|
+    file_names.delete(blacklisted)
+  end
+  "app/assets/images/rotate/{files.shuffle.first}"
+end
 end
