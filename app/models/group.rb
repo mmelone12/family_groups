c45cccbae184e95ad3eb6d1a87a114b79ba278de
mmelone12 <matt.m.melone@gmail.com>
2013-09-24 21:17:50 -0700

    still fiddling

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,12 +5,12 @@ class Group < ActiveRecord::Base
 	validates :desc, presence: true, length: { maximum: 230}
 	mount_uploader :image_path, ImageUploader
 
-	RMeetup::Client.api_key = "2e2c342c1e7b93a141362e4427b7"
-
 	geocoded_by :address do |user,results|
     	if geo = results.first
       		user.city = geo.city
     	end
   	end
   	after_validation :geocode, :if => :address_changed?
+
+  	RMeetup::Client.api_key = "2e2c342c1e7b93a141362e4427b7"
 end
