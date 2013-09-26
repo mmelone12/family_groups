a2fad94ac1d7925577d1f217490af65950c990a7
mmelone12 <matt.m.melone@gmail.com>
2013-09-25 20:33:45 -0700

    still fooling around with it

--- config/initializers/apis.rb
+++ config/initializers/apis.rb
@@ -1,10 +1,12 @@
 RMeetup::Client.api_key = "2e2c342c1e7b93a141362e4427b7"
 
-module APIS
+module AOK
+  module APIS
 	module_function
 
-		def meetup
-			@meetup ||= Rmeetup::Client
-		end
-	end
+	  def meetup
+		@meetup ||= RMeetup::Client
+	  end
+
+  end
 end
