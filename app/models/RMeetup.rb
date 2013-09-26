23ef911b3aa9deff39c833e69d9d1a54992c8b8c
mmelone12 <matt.m.melone@gmail.com>
2013-09-26 10:34:29 -0700

    crazy things in Rmeetup.rb

--- app/models/RMeetup.rb
+++ app/models/RMeetup.rb
@@ -1,3 +1,11 @@
 class RMeetup < ActiveRecord::Base
-Client.api_key = "2e2c342c1e7b93a141362e4427b7"
+ 
+ require "rMeetup"
+
+  API_KEY = "2e2c342c1e7b93a141362e4427b7"
+
+  def new_groups
+	Client.api_key = "API_KEY"
+	@gresults = Client.fetch(:groups, :lat => @user.latitude, :lon => @user.longitude, :topic => "parents")
+  end
 end
