288fd2a71fb765782c81c553059598df51880e5b
mmelone12 <matt.m.melone@gmail.com>
2013-11-10 14:53:17 -0800

    fixed a couple models

--- app/models/group.rb
+++ app/models/group.rb
@@ -21,7 +21,7 @@ class Group < ActiveRecord::Base
   	after_validation :geocode, :if => :address_changed?
 
   def image_if_blank
-    if uploader_image.blank and image_path.blank?
+    if uploader_image.blank? and image_path.blank?
       self.image_path = "rotate/shutterstock_16170193.jpg"
     end
   end
