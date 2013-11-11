880dd6d6681fca38dbbe553ba1ae0e7db367e070
mmelone12 <matt.m.melone@gmail.com>
2013-11-11 11:50:16 -0800

    maybe mail works now?

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,10 +2,10 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",  
   :port                 => 587,  
   :domain               => "familygroups.org",  
-  :user_name            => "contact",  
+  :user_name            => "contact@familygroups.org",  
   :password             => "seesaw1985!",  
   :authentication       => "plain",  
-  :enable_starttls_auto => true   
+ :enable_starttls_auto => true   
 }
 
-ActionMailer::Base.default_url_options[:host] = "http://familygroups.org"
+ActionMailer::Base.default_url_options[:host] = "familygroups.org"
