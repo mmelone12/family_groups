a492196ceffe5b26053d5d8d246e20829910fa9b
mmelone12 <matt.m.melone@gmail.com>
2013-12-05 21:25:37 -0800

    fixed up meta tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,8 @@ module ApplicationHelper
 	def title(page_title)
   		content_for :title, page_title.to_s
 	end
+
+  def description(page_description)
+      content_for :description, page_description.to_s
+  end
 end
